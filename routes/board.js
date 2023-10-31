42b6d486e05b9277b608a92c5f57258c73fd38e7
Prameela2511 <143015943+Prameela2511@users.noreply.github.com>
2023-10-30 21:31:00 -0500

    made changes

--- routes/board.js
+++ routes/board.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Board Display' });
-});
 
-module.exports = router;
+router.get('/', function(req, res, next) {
+    res.render('board', { title: 'Board Display' });
+    let query = req.query
+    console.log(`rows ${query.rows}`)
+    console.log(`cols ${query.cols}`)
+    res.render('board', {title: 'Board Display', query : query})
+  });  
+  module.exports = router;
